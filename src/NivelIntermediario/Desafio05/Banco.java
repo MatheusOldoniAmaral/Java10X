3fb65e85025d872c63dc9910bbbf179bd35911c8
Matheus Oldoni Amaral <matheusoldoniamaral2003@gmail.com>
2026-09-04 00:18:41 -0300

    feat: adiciona classe Banco com lista de contas

--- src/NivelIntermediario/Desafio05/Banco.java
+++ src/NivelIntermediario/Desafio05/Banco.java
@@ -1,4 +1,25 @@
 package NivelIntermediario.Desafio05;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Banco {
+
+    private List<ContaBancaria> contas;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(ContaBancaria conta) {
+        contas.add(conta);
+    }
+
+    public void transferir(ContaBancaria origem, ContaBancaria destino, double valor) {
+
+    }
+
+    public void transferir(int indiceOrigem, int indiceDestino, double valor) {
+
+    }
 }
